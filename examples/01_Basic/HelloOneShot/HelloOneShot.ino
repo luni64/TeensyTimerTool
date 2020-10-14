36f7fcd5a2e5ee42812cd8f44b52d6667bdc3cc1
luni64 <lutz.niggl@lunoptics.com>
2020-10-14 18:50:45 +0200

    Restructured examples folder

--- examples/01_Basic/HelloOneShot/HelloOneShot.ino
+++ examples/01_Basic/HelloOneShot/HelloOneShot.ino
@@ -9,18 +9,18 @@ void callback()
 }
 
 
-OneShotTimer t1; // generate a timer from the pool (Pool: 2xGPT, 16xTMR(QUAD), 20xTCK)
+OneShotTimer timer1; // generate a timer from the pool (Pool: 2xGPT, 16xTMR(QUAD), 20xTCK)
 
 void setup()
 {
     pinMode(LED_BUILTIN,OUTPUT);
-    t1.begin(callback);
+    timer1.begin(callback);
 }
 
 void loop()
 {
     digitalWriteFast(LED_BUILTIN, HIGH);
-    t1.trigger(10'000); // switch of after 10ms
+    timer1.trigger(10'000); // switch of after 10ms
 
     delay(500);
 }
