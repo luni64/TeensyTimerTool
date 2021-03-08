8de224ba03921231c97bb68e4a1e412e74a1e35f
luni64 <lutz.niggl@lunoptics.com>
2021-03-08 07:11:20 +0100

    Changed flag re-reading to DSB for performance reasons  (thanks LAtimes)
    https://forum.pjrc.com/threads/66433-Teensy-4-Timed-Interrupts-faster-than-one-microsecond?p=272725&viewfull=1#post272725

--- src/Teensy/GPT/GPT.h
+++ src/Teensy/GPT/GPT.h
@@ -34,17 +34,17 @@ namespace TeensyTimerTool
         {
             isInitialized = true;
 
-            if (moduleNr == 0) // GPT1 clock settings
-                CCM_CCGR1 |= CCM_CCGR1_GPT1_BUS(CCM_CCGR_ON) | CCM_CCGR1_GPT1_SERIAL(CCM_CCGR_ON);
-            else // GPT2
-                CCM_CCGR0 |= CCM_CCGR0_GPT2_BUS(CCM_CCGR_ON) | CCM_CCGR0_GPT2_SERIAL(CCM_CCGR_ON);
-
-            if(USE_GPT_PIT_150MHz) // timer clock setting from config.h
-                CCM_CSCMR1 &= ~CCM_CSCMR1_PERCLK_CLK_SEL; // 150MHz
-            else
-                CCM_CSCMR1 |= CCM_CSCMR1_PERCLK_CLK_SEL;  // 24MHz
-
-            pGPT->CR = GPT_CR_CLKSRC(0x001) | GPT_CR_ENMOD; // stopped, restart mode and peripheral clock
+            if (moduleNr == 0)                                                                     // GPT1 clock settings
+                CCM_CCGR1 |= CCM_CCGR1_GPT1_BUS(CCM_CCGR_ON) | CCM_CCGR1_GPT1_SERIAL(CCM_CCGR_ON); //
+            else                                                                                   // GPT2
+                CCM_CCGR0 |= CCM_CCGR0_GPT2_BUS(CCM_CCGR_ON) | CCM_CCGR0_GPT2_SERIAL(CCM_CCGR_ON); //
+                                                                                                   //
+            if (USE_GPT_PIT_150MHz)                                                                // timer clock setting from config.h
+                CCM_CSCMR1 &= ~CCM_CSCMR1_PERCLK_CLK_SEL;                                          // use F_BUS
+            else                                                                                   //
+                CCM_CSCMR1 |= CCM_CSCMR1_PERCLK_CLK_SEL;                                           // use 24MHz
+                                                                                                   //
+            pGPT->CR = GPT_CR_CLKSRC(0x001) | GPT_CR_ENMOD;                                        // stopped, restart mode and peripheral clock source
 
             attachInterruptVector(irq, isr);
             NVIC_ENABLE_IRQ(irq);
@@ -60,10 +60,10 @@ namespace TeensyTimerTool
     {
         if (!channel->periodic)
             pGPT->CR &= ~GPT_CR_EN; // stop timer in one shot mode
-
-        pGPT->SR = 0x3F; // reset all interrupt flags
-        callback();      // we only enabled the OF1 interrupt-> no need to find out which interrupt was actually called
-        pGPT->SR;                 // re-read flag to prevent re entering, takes very long, seems to work without?
+                                    //
+        pGPT->SR = 0x3F;            // reset all interrupt flags
+        callback();                 // we only enabled the OF1 interrupt-> no need to find out which interrupt was actually called
+        asm volatile("dsb");        // wait until register changes propagated through the cache
     }
 
     template <unsigned m>
