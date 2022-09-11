d46b0a59c8d7081f18ab502109c2d11deab93aae
luni64 <12611497+luni64@users.noreply.github.com>
2022-09-11 18:13:18 +0200

    Added simple examples test file

---
+++ test/main.cpp
@@ -0,0 +1,17 @@
+#include "Arduino.h"
+
+// General =============================================================================================
+//#include "../lib/TeensyTimerTool/examples/01_Basic/HelloPeriodic/HelloPeriodic.ino"
+//#include "../lib/TeensyTimerTool/examples/01_Basic/HelloOneShot/HelloOneShot.ino"
+//#include "../lib/TeensyTimerTool/examples/01_Basic/UsingChronoDurations1/UsingChronoDurations1.ino"
+//#include "../lib/TeensyTimerTool/examples/01_Basic/UsingChronoDurations2/UsingChronoDurations2.ino"
+//#include "../lib/TeensyTimerTool/examples/02_Advanced/CallbackWithParams/CallbackWithParams.ino"
+//#include "../lib/TeensyTimerTool/examples/02_Advanced/UsingLambdas/UsingLambdas.ino"
+
+//T3.x ================================================================================================
+//#include "../lib/TeensyTimerTool/examples/03_Applications/DoubleExposure/DoubleExposure.ino"
+
+//T4.x ================================================================================================
+//#include "../lib/TeensyTimerTool/examples/01_Basic/MoreTimers/MoreTimers.ino"
+//#include "../lib/TeensyTimerTool/examples/01_Basic/RTC_Timer/RTC_Timer.ino"
+//#include "../lib/TeensyTimerTool/examples/99_Misc/PinInformation/PinInformation.ino"
