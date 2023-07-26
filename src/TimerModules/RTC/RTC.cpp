becffe83efc1ab061ae11ad99c7d7c57820c075d
luni64 <lutz@niggl-schlagbauer.de>
2023-07-26 11:51:41 +0200

    Removed c++17 dependency  (inline variables)

---
+++ src/TimerModules/RTC/RTC.cpp
@@ -0,0 +1,30 @@
+#include "RTC.h"
+#include "RtcChannel.h"
+
+namespace TeensyTimerTool::RTC_t
+{
+    namespace // private
+    {
+        callback_t callback = nullptr;
+        RtcChannel channel(&callback);
+        bool initialized = false;
+
+        void isr()
+        {
+            SNVS_HPSR |= 0b11; // reset interrupt flag
+            if (callback != nullptr) callback();
+            asm volatile("DSB");
+        }
+    }
+
+    ITimerChannel *getTimer()
+    {
+        if (!initialized)
+        {
+            attachInterruptVector(IRQ_SNVS_IRQ, isr);
+            NVIC_ENABLE_IRQ(IRQ_SNVS_IRQ);
+            initialized = true;
+        }
+        return &channel;
+    }
+} // namespace TeensyTimerTool::RTC_t
--- src/TimerModules/RTC/RTC.h
+++ src/TimerModules/RTC/RTC.h
@@ -1,32 +1,8 @@
 #pragma once
 
-#include "RtcChannel.h"
+#include "ITimerChannel.h"
 
-namespace TeensyTimerTool
+namespace TeensyTimerTool::RTC_t
 {
-    namespace RTC_t
-    {
-        callback_t callback = nullptr;
-        inline RtcChannel channel(&callback);
-
-        void isr()
-        {
-            SNVS_HPSR |= 0b11; // reset interrupt flag
-            if (callback != nullptr) callback();
-            asm volatile("DSB");
-        }
-
-        ITimerChannel *getTimer()
-        {
-            static bool initialized = false;
-            if (!initialized)
-            {
-                attachInterruptVector(IRQ_SNVS_IRQ, isr);
-                NVIC_ENABLE_IRQ(IRQ_SNVS_IRQ);
-            }
-            return &channel;
-        }
-
-    } // namespace RTC_t
-
-} // namespace TeensyTimerTool
+    extern ITimerChannel *getTimer();
+}
